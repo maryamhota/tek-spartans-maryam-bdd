4a90ad4eda9c91b6971b33f2a6ff251ec558bebb
maryamhotak <mhotak.us@gmail.com>
2024-07-31 17:14:24 -0500

    Setup class completed

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,33 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+
+    // we need to make it an instance variable not local variable, outside the methode
+    private static WebDriver driver;// we need to make it static to make it shareable
+    public void setupBrowser (){
+
+        // Web driver instance then Navigation to retail app
+        // maximize and implicitly wait
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+    }
+    public void quitBrowser (){
+        // null check to avoid exceptions
+        if ( driver != null){ // ! = if it's not null, then we won't do anything
+            driver.quit();
+        }
+    }
+
+    // we need to encapsulate the code and give read only indirect access to driver
+    public WebDriver getDriver (){
+        return driver;// since its static we dont need to use the .this keyword
+    }
+}
